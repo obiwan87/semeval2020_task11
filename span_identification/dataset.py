0dabffbf8b2bfd1119797e33ab30b73571e09408
Simone Di Stefano <simone.distefano@merkurtz.info>
2022-08-02 15:06:49 +0200

    Use utf-8 for File I/O

--- span_identification/dataset.py
+++ span_identification/dataset.py
@@ -19,7 +19,7 @@ def load_data(data_folder, propaganda_techniques_file):
 
     with open(propaganda_techniques_file, "r") as f:
         propaganda_techniques_names = [line.rstrip() for line in f.readlines()]
-    
+
     return articles_content, articles_id, propaganda_techniques_names
 
 
@@ -42,12 +42,12 @@ def group_spans_by_article_ids(span_list):
     return data
 
 
-def get_train_dev_files(articles_id, articles_content, nlp, labels_path, train_file, dev_file, split_by_ids=True, 
-                     dev_size=0.3, random_state=42):
+def get_train_dev_files(articles_id, articles_content, nlp, labels_path, train_file, dev_file, split_by_ids=True,
+                        dev_size=0.3, random_state=42):
     articles_content_dict = dict(zip(articles_id, articles_content))
     articles_id, gold_spans = read_predictions_from_file(labels_path)
     span_list = list(zip(articles_id, gold_spans))
-    
+
     if split_by_ids:
         data = group_spans_by_article_ids(span_list)
         train_ids, dev_ids = train_test_split(np.unique(articles_id), test_size=dev_size, random_state=random_state)
@@ -59,16 +59,16 @@ def get_train_dev_files(articles_id, articles_content, nlp, labels_path, train_f
         dev_data = sorted(group_spans_by_article_ids(span_list_train).items())
         train_ids = [example[0] for example in train_data]
         dev_ids = [example[0] for example in dev_data]
-    
+
     create_BIO_labeled(train_file, train_data, articles_content_dict, nlp)
     create_BIO_labeled(dev_file, dev_data, articles_content_dict, nlp)
-    
+
     return train_ids, dev_ids
-    
-                    
+
+
 def get_test_file(file, articles_id, articles_content, nlp):
     create_BIO_unlabeled(file, articles_id, articles_content, nlp)
-    
+
 
 def token_label_from_spans(pos, spans):
     for el in spans:
@@ -76,28 +76,33 @@ def token_label_from_spans(pos, spans):
             return "PROP"
     return 'O'
 
-                    
+
 def create_BIO_labeled(file, data, articles_content_dict, nlp):
     prev_label = 'O'
-    with open(file, 'w') as f:
+    with open(file, 'w', encoding='utf-8') as f:
         for article_id, spans in tqdm(data):
             text = articles_content_dict[article_id]
             tokens = [(token.idx, token.text) for token in nlp(text)]
-            idx = np.array(tokens)[:,0]
-            tokens = np.array(tokens)[:,1]
+            idx = np.array(tokens)[:, 0]
+            tokens = np.array(tokens)[:, 1]
             prev_tok = '\n'
-            
+
             for i in range(len(tokens)):
                 tok = tokens[i].replace('\n', ' ').replace('\t', ' ').strip()
                 if len(tok) != 0 and repr(tok) != repr('\ufeff') and repr(tok) != repr('\u200f'):
                     tok = tokens[i].strip().replace('\n', ' ').replace('\t', ' ')
-                    label =  token_label_from_spans(idx[i], spans)
+                    label = token_label_from_spans(idx[i], spans)
                     if label != 'O':
                         if prev_label != 'O':
                             label = 'I-' + 'PROP'
                         else:
                             label = 'B-' + 'PROP'
-                    f.write(tok + '\t' + label + '\n')
+                    try:
+                        f.write(tok + '\t' + label + '\n')
+                    except UnicodeEncodeError as e:
+                        print(e.reason)
+                        print("tok:", tok, "label: ", label)
+                        raise e
                     prev_label = label
                     prev_tok = tok
                 else:
@@ -106,16 +111,16 @@ def create_BIO_labeled(file, data, articles_content_dict, nlp):
                         prev_tok = '\n'
                     prev_label = 'O'
 
-                    
+
 def create_BIO_unlabeled(file, articles_id, articles_content, nlp):
     prev_label = 'O'
-    with open(file, 'w') as f:
+    with open(file, 'w', encoding='utf-8') as f:
         for article_id, text in tqdm(zip(articles_id, articles_content)):
             tokens = [(token.idx, token.text) for token in nlp(text)]
-            idx = np.array(tokens)[:,0]
-            tokens = np.array(tokens)[:,1]
+            idx = np.array(tokens)[:, 0]
+            tokens = np.array(tokens)[:, 1]
             prev_tok = '\n'
-            
+
             for i in range(len(tokens)):
                 tok = tokens[i].replace('\n', ' ').replace('\t', ' ').strip()
                 if len(tok) != 0 and repr(tok) != repr('\ufeff') and repr(tok) != repr('\u200f'):
@@ -130,7 +135,7 @@ def create_BIO_unlabeled(file, articles_id, articles_content, nlp):
                         prev_tok = '\n'
                     prev_label = 'O'
 
-                    
+
 def create_subfolder(subfolder, source_folder, articles_id):
     if os.path.exists(subfolder):
         rmtree(subfolder)
